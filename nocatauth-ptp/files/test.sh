19adc50724f4f67f2cd2c51150c6b78983874989
Keegan Quinn <keeganquinn@gmail.com>
2012-02-22 01:54:04 -0800

    Updated test script.

--- nocatauth-ptp/files/test.sh
+++ nocatauth-ptp/files/test.sh
@@ -1,4 +1,3 @@
 #/bin/sh
 find lib -name '*.pm' | while read i; do perl -cwIlib $i; done
-find bin/{gateway,admintool} cgi-bin -perm +0111 -type f | \
-    while read i; do perl -cwIlib $i; done
+perl -cwIlib bin/gateway
