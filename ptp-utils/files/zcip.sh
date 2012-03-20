f7899da2a38bf6075f1c7b114fab26dd278acaf8
Keegan Quinn <keeganquinn@gmail.com>
2012-03-20 15:58:22 -0700

    Cleaned up tabulation.

--- ptp-utils/files/zcip.sh
+++ ptp-utils/files/zcip.sh
@@ -2,18 +2,18 @@
 # Copyright 2012 Personal Telco Project
 
 stop_interface_zcip() {
-        service_stop /sbin/zcip
+    service_stop /sbin/zcip
 }
 
 setup_interface_zcip() {
-	local iface="$1"
-	local config="$2"
+    local iface="$1"
+    local config="$2"
 
-	local mtu
-	config_get mtu "$config" mtu 1492
+    local mtu
+    config_get mtu "$config" mtu 1492
 
-        ifconfig "$iface" mtu "$mtu"
+    ifconfig "$iface" mtu "$mtu"
 
-        service_start /sbin/zcip "$iface" /usr/share/zcip/zcip.script
+    service_start /sbin/zcip "$iface" /usr/share/zcip/zcip.script
 }
 
